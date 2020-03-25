d7d0c8cf03336d6bfa281d35563788754b180e2e
Jaime Gensler <jaimegensler0@gmail.com>
2020-03-25 09:45:40 -0700

    Fixes

--- db/students.js
+++ db/students.js
@@ -1,10 +1,14 @@
-import tableMaker from './tableMaker';
+import tableReader from './tableReader';
 
-let students = tableMaker`
- ID | name          | classStanding | majors        | minors         | tutorTypes
-----+---------------+---------------+---------------+----------------+------------
-  1 | Jaime Gensler | 4             | [ ENG, PHIL ] | [ CW ]         | [ C, W ]
-  2 | Grace Krueger | 3             | [ THAR    ]   | [ SPAN, LING ] | [ W ]
+let students = tableReader`
+ ID | name              | classStanding | majors         | minors         | tutorTypes
+----+-------------------+---------------+----------------+----------------+------------
+  1 | Vape Juice Jaime  | 4             | [ ENG, PHIL ]  | [ CW ]         | [ C, W ]
+  2 | Vape Juice Jordan | 1             | [ CMSC ]       | [ BEIN, DUMB ] | [ C ]
+  3 | Vape Juice Josh   | 2             | [ MATH ]       | [ BEIN, LAME ] | [ C ]
+  4 | Grape Juice Grace | 3             | [ THAR ]       | [ SPAN, LING ] | [ C ]
+  5 | Ryan Gosling      | 4             | [ THAR, GOVT ] | [ MUTH ]       | [ ]
+  6 | John Krasinski    | 2             | [ PSYC ]       | [ ]            | [ ]
 `;
 
 export default students;
---
+++ db/tableReader.js
@@ -0,0 +1,32 @@
+function tableReader(table) {
+    const [heading, body] = table[0].split(/^[-+]+$/m);
+    const keys = heading.trim().split(/[ |]+/);
+    const rows = body.trim().split('\n');
+
+    return rows
+        .filter(row => !/^\/\//.test(row.trim()))
+        .map(row => {
+            const vals = row.trim().split(/ *\| */);
+
+            return keys.reduce((acc, key, i) => {
+                const raw = vals[i];
+                let val;
+
+                if (/^\d+$/.test(raw)) {
+                    val = parseFloat(raw);
+                } else if (/\[.*\]/.test(raw)) {
+                    val = raw
+                        .replace(/\[|\]/g, '')
+                        .trim()
+                        .split(/, ?/)
+                        .filter(e => e !== '');
+                } else {
+                    val = raw;
+                }
+
+                return { ...acc, [key]: val };
+            }, {});
+        });
+}
+
+export default tableReader;
